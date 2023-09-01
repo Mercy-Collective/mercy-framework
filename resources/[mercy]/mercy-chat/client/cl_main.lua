4175b26492f72e1495ff93264cd391750212842e
PuzzleTV <84552359+PuzzleTV@users.noreply.github.com>
2023-09-01 13:33:28 -0400

    Hotfix: Showing 2 times IdCard

--- resources/[mercy]/mercy-chat/client/cl_main.lua
+++ resources/[mercy]/mercy-chat/client/cl_main.lua
@@ -88,7 +88,7 @@ RegisterNetEvent('mercy-chat/client/send-identification', function(CitizenId, Fi
     local ClosestPlayer = PlayerModule.GetClosestPlayer(nil, 2.0)
 
     if ClosestPlayer['ClosestPlayerPed'] == -1 and ClosestPlayer['ClosestServer'] == -1 then
-        EventsModule.TriggerServer("mercy-items/server/show-identification", CitizenId, Firstname, Lastname, Date, Sex)
+        EventsModule.TriggerServer("mercy-items/server/show-identification", CitizenId, Firstname, Lastname, Date, Sex, nil)
     else
         EventsModule.TriggerServer("mercy-items/server/show-identification", CitizenId, Firstname, Lastname, Date, Sex, ClosestPlayer['ClosestServer'])
     end
