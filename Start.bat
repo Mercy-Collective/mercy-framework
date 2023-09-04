21ff463ea0130ac27fae07c6d277a4b9e8be6fd7
AwayFromKane <40138067+AwayFromKane@users.noreply.github.com>
2023-09-05 01:48:54 +0200

    Starter Changes

--- Start.bat
+++
@@ -1,1 +0,0 @@
-C:\Users\Administrator\Desktop\Mercy-Framework\artifacts\FXServer.exe +exec cfg/server.cfg
---
+++ [Starter].bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+color 09
+ECHO Thanks for using Mercy Framework, we hope you enjoy it!
+timeout /t 2 /nobreak >nul
+ECHO Starting Mercy Framework...
+timeout /t 3 /nobreak >nul
+color 07
+cls
+
+C:\Users\Administrator\Desktop\mercy-frameworkk\artifacts\FXServer.exe +exec cfg/server.cfg
